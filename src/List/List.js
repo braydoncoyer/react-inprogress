4d59a8de8fb92f3adcac9a61bfdf9760165ef11b
Braydon Coyer <braydon@coyer.net>
2020-08-19 10:49:25 -0500

    Including files

---
+++ src/List/List.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import './list.css';
+
+const List = () => {
+
+    const [items, setItems] = useState([
+        {text: "Learn Angular"},
+        {text: "Learn React"},
+        {text: "Learn Vue"}
+    ]);
+
+  return (
+      <div className="list__container">
+          <div className="list__container-content">
+              <h1 className="list__container-title">Reminders</h1>
+              {
+                  items.map((item, index) => (
+                      <h4 key={index}>{item.text}</h4>
+                  ))
+              }
+          </div>
+      </div>
+  )
+
+};
+
+export default List;
